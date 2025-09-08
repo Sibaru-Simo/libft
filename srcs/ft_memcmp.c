d6a392f9a43a14d6e8bc1f03a820ab3f18e4402f
Sibaru-simo <s9ymosimo@gmail.com>
2025-09-08 23:33:04 +0100

    UPDATE

---
+++ srcs/ft_memcmp.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sidna_7 <who??@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/08 23:20:03 by sidna_7           #+#    #+#             */
+/*   Updated: 2025/09/08 23:31:53 by sidna_7          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const unsigned char	*p1;
+	const unsigned char	*p2;
+	size_t				i;
+
+	i = 0;
+	p1 = s1;
+	p2 = s2;
+	if (n == 0)
+		return (0);
+	while (i < n)
+	{
+		if (p1[i] != p2[i])
+			return ((unsigned char)p1[i] - (unsigned char)p2[i]);
+		i++;
+	}
+	return (0);
+}
