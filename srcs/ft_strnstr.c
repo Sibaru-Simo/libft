61165ca5359bc9c06e5dbe364e7b6178d80e4052
Sibaru-simo <s9ymosimo@gmail.com>
2025-09-09 00:41:04 +0100

    UPDATE

---
+++ srcs/ft_strnstr.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sidna_7 <who??@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/09 00:21:20 by sidna_7           #+#    #+#             */
+/*   Updated: 2025/09/09 00:32:12 by sidna_7          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include "libft.h"
+
+char	*ft_strnstr(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (!s2[i])
+		return ((char *)s1);
+	while (s1[i] && i < n)
+	{
+		j = 0;
+		while (s1[i + j] == s2[j] && j + i < n)
+			j++;
+		if (s2[j] == '\0')
+			return ((char *)&s1[i]);
+		i++;
+	}
+	return (NULL);
+}
